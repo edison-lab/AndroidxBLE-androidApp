257cdabd1ee0382c967f5a93c7a88f612ec0add7
satoyap <yap@yap-no-MacBook-Air.local>
2015-08-18 14:51:06 +0900

    AndoidxBLE-androidApp
    AndoidxBLE-androidApp

--- BLE_TempSensor/app/src/main/java/jp/kaibutsu/ble_tempsensor/MainActivity.java
+++ BLE_TempSensor/app/src/main/java/jp/kaibutsu/ble_tempsensor/MainActivity.java
@@ -3,6 +3,8 @@ package jp.kaibutsu.ble_tempsensor;
 
 import java.util.UUID;
 
+import android.support.v7.app.AppCompatActivity;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -19,22 +21,23 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
     //scan timeout
     private static final long SCAN_PERIOD = 10000;
-    //devicename
-    private static final String DEVICE_NAME = "Edison";
-    //service_UUID
-    private static final String DEVICE_SERVICE_UUID = "00002800-0000-1000-8000-00805f9b34fb";
-    //characteristic_UUID
-    private static final String DEVICE_CHARACTERISTIC_UUID = "00003333-0000-1000-8000-00805f9b34fb";
 
-    private static final String TAG = "BLE_temp";
+    //uuid
+    private static final String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    private static final String DEVICE_NAME = "Edison";
+    private static final String DEVICE_BRIL_SERVICE_UUID = "00002800-0000-1000-8000-00805f9b34fb";
+    private static final String DEVICE_RDWT_CHARACTERISTIC_UUID = "00003333-0000-1000-8000-00805f9b34fb";
+    private static final String DEVICE_NOTIFY_CHARACTERISTIC_UUID = "00003333-0000-1000-8000-00805f9b34fb";
+
+
+    private static final String TAG = "BLE_Edison";
 
     private BleStat mStatus = BleStat.INIT;
     private Handler mHandler;
@@ -47,28 +50,39 @@ public class MainActivity extends Activity {
     private TextView mTextStatus;
     private TextView mTextRead;
     private Button mBtnConn, mBtnDiscon;
-    private Button mBtnUpdate;
+    private Button mbtnWrite;
     private String mReadData = "";
+    private Button mBtnDisable, mBtnEnable;
+
 
     public View.OnClickListener BtnClickListener = new View.OnClickListener() {
-
-        //ボタンIDで区別する
 
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btnConn:
-                    Log.e(TAG, "Connect pressed!");
+                    Log.e(TAG, "button Connect");
                     connect();
                     break;
                 case R.id.btnDisConn:
-                    Log.e(TAG, "Disconnect pressed");
+                    Log.e(TAG, "button Disconnect");
                     disconnect();
                     break;
-                case R.id.btnRead:
-                    Log.e(TAG, "UPDATE pressed");
-                    ReadGatt();
+                case R.id.btnWrite:
+                    Log.e(TAG, "button Write");
+                    WriteData();
                     break;
+
+                case R.id.btnDisable:
+                    Log.e(TAG, "button notify_disconnect");
+                    DisNotificationGatt();
+                    break;
+
+                case R.id.btnEnable:
+                    Log.e(TAG, "button notify_connect");
+                    NotificationGatt();
+                    break;
+
             }
         }
     };
@@ -85,19 +99,30 @@ public class MainActivity extends Activity {
             finish();
         }
 
-        mTextStatus = (TextView) findViewById(R.id.textState);
+        mTextStatus = (TextView) findViewById(R.id.textView1);
         mTextRead = (TextView) findViewById(R.id.textView2);
         mBtnConn = (Button) findViewById(R.id.btnConn);
         mBtnDiscon = (Button) findViewById(R.id.btnDisConn);
-        mBtnUpdate = (Button) findViewById(R.id.btnRead);
+        mbtnWrite = (Button) findViewById(R.id.btnWrite);
+
+        mBtnDisable = (Button) findViewById(R.id.btnDisable);
+        mBtnEnable = (Button) findViewById(R.id.btnEnable);
+
 
         mBtnConn.setOnClickListener(BtnClickListener);
         mBtnDiscon.setOnClickListener(BtnClickListener);
-        mBtnUpdate.setOnClickListener(BtnClickListener);
+        mbtnWrite.setOnClickListener(BtnClickListener);
+
+        mBtnDisable.setOnClickListener(BtnClickListener);
+        mBtnEnable.setOnClickListener(BtnClickListener);
+
 
         mBtnConn.setEnabled(true);
         mBtnDiscon.setEnabled(false);
-        mBtnUpdate.setEnabled(false);
+        mbtnWrite.setEnabled(false);
+
+        mBtnDisable.setEnabled(false);
+        mBtnEnable.setEnabled(false);
 
         mHandler = new Handler() {
             @Override
@@ -107,23 +132,33 @@ public class MainActivity extends Activity {
                 if (msg.obj.toString().equals(BleStat.READY.toString())) {
                     mBtnConn.setEnabled(false);
                     mBtnDiscon.setEnabled(true);
-                    mBtnUpdate.setEnabled(true);
+                    mbtnWrite.setEnabled(true);
+
+                    mBtnDisable.setEnabled(true);
+                    mBtnEnable.setEnabled(true);
                 }
                 if (msg.obj.toString().equals(BleStat.BUSY.toString())) {
-                    mBtnUpdate.setEnabled(false);
+                    mbtnWrite.setEnabled(false);
                 }
                 if (msg.obj.toString().equals(BleStat.CLOSED.toString())) {
                     mBtnConn.setEnabled(true);
                     mBtnDiscon.setEnabled(false);
-                    mBtnUpdate.setEnabled(false);
+                    mbtnWrite.setEnabled(false);
+
+                    mBtnDisable.setEnabled(false);
+                    mBtnEnable.setEnabled(false);
                 }
                 if (msg.obj.toString().equals(BleStat.DISCONNECTED.toString())) {
                     mBtnConn.setEnabled(true);
                     mBtnDiscon.setEnabled(false);
-                    mBtnUpdate.setEnabled(false);
+                    mbtnWrite.setEnabled(false);
+
+                    mBtnDisable.setEnabled(false);
+                    mBtnEnable.setEnabled(false);
+
                 }
                 if (msg.obj.toString().equals(BleStat.DATA_UPDATE.toString())) {
-                    mBtnUpdate.setEnabled(true);
+                    mbtnWrite.setEnabled(true);
 
                     mTextRead.setText(mReadData + " ℃");
                 }
@@ -144,6 +179,8 @@ public class MainActivity extends Activity {
         SERVICE_NOT_FOUND,
         SERVICE_FOUND,
         CHARACTERISTIC_NOT_FOUND,
+        NOTIFICATION_REGISTERED,
+        NOTIFICATION_REGISTER_FAILED,
         DATA_UPDATE,
         READY,
         BUSY,
@@ -157,11 +194,51 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void NotificationGatt() {
+        boolean registered = mGatt.setCharacteristicNotification(mDataCharacteristic, true);
+        BluetoothGattDescriptor descriptor = mDataCharacteristic.getDescriptor(
+                UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        mGatt.writeDescriptor(descriptor);
+        if (registered) {
+            setStatus(BleStat.NOTIFICATION_REGISTERED);
+            Log.e(TAG, "Notify_Characteristics通知設定: 完了");
 
-    private void ReadGatt() {
-        if (mDataCharacteristic != null) {
-            mGatt.readCharacteristic(mDataCharacteristic);
+        } else {
+            setStatus(BleStat.NOTIFICATION_REGISTER_FAILED);
+            Log.e(TAG, "Notify_Characteristics通知設定: エラー");
         }
+
+    }
+
+    private void DisNotificationGatt() {
+        BluetoothGattDescriptor descriptor = mDataCharacteristic.getDescriptor(
+                UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+        descriptor.setValue(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE);
+        mGatt.writeDescriptor(descriptor);
+
+        boolean registered = mGatt.setCharacteristicNotification(mDataCharacteristic, false);
+        if (registered) {
+            setStatus(BleStat.NOTIFICATION_REGISTERED);
+            Log.e(TAG, "Notify_Characteristics通知設定: 解除");
+
+        } else {
+            setStatus(BleStat.NOTIFICATION_REGISTER_FAILED);
+            Log.e(TAG, "Notify_Characteristics通知設定: エラー");
+        }
+
+    }
+
+    private void WriteData() {
+        if (mDataCharacteristic != null) {
+
+            mDataCharacteristic.setValue("1");
+
+            boolean status = mGatt.writeCharacteristic(mDataCharacteristic);
+            if (status == false) {
+            Log.e(TAG, "writeCharacteristic failed");
+        }
+    }
     }
 
     private void connect() {
@@ -182,6 +259,7 @@ public class MainActivity extends Activity {
     }
 
     private void disconnect() {
+
         if (mBluetoothGatt != null) {
             mBluetoothGatt.close();
             mBluetoothGatt = null;
@@ -211,11 +289,10 @@ public class MainActivity extends Activity {
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             Log.d(TAG, "onConnectionStateChange: " + status + " -> " + newState);
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-
                 gatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-
                 setStatus(BleStat.DISCONNECTED);
+                setTitle("BLE-edison - DISCONNECTED");
                 mBluetoothGatt = null;
             }
         }
@@ -224,51 +301,77 @@ public class MainActivity extends Activity {
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             Log.d(TAG, "onServicesDiscovered received: " + status);
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                BluetoothGattService service = gatt.getService(UUID.fromString(DEVICE_SERVICE_UUID));
+                BluetoothGattService service = gatt.getService(UUID.fromString(DEVICE_BRIL_SERVICE_UUID));
                 if (service == null) {
                     setStatus(BleStat.SERVICE_NOT_FOUND);
+                    setTitle("BLE-edison - SERVICE_NOT_FOUND");
 
                 } else {
                     setStatus(BleStat.SERVICE_FOUND);
-
+                    setTitle("BLE-edison - SERVICE_FOUND");
                     mDataCharacteristic =
-                            service.getCharacteristic(UUID.fromString(DEVICE_CHARACTERISTIC_UUID));
+                            service.getCharacteristic(UUID.fromString(DEVICE_RDWT_CHARACTERISTIC_UUID));
                     if (mDataCharacteristic == null) {
                         setStatus(BleStat.CHARACTERISTIC_NOT_FOUND);
-
+                        setTitle("BLE-edison - CHARACTERISTIC_NOT_FOUND");
                     } else {
 
                         mGatt = gatt;
                         setStatus(BleStat.READY);
-                        ReadGatt();
+
+                        Log.e(TAG, "check permission");
+                        Log.e(TAG, "characteristic prop " + mDataCharacteristic.getProperties());
+                        Log.e(TAG, "characteristic perm " + mDataCharacteristic.getPermissions());
                     }
                 }
             }
         }
 
-
         @Override
-        public void onCharacteristicRead(BluetoothGatt gatt,
-                                         BluetoothGattCharacteristic characteristic,
-                                         int status) {
+        public void onCharacteristicWrite(BluetoothGatt gatt,
+                                          BluetoothGattCharacteristic characteristic, int status) {
+            Log.d(TAG, "onCharacteristicWrite: " + status);
+            switch (status) {
+                case BluetoothGatt.GATT_SUCCESS:
+                    Log.e(TAG, "onCharacteristicWrite: GATT_SUCCESS");
+                    break;
+                case BluetoothGatt.GATT_WRITE_NOT_PERMITTED:
+                    Log.e(TAG, "onCharacteristicWrite: GATT_WRITE_NOT_PERMITTED");
+                    break;
 
-            Log.d(TAG, "onCharacteristicRead: " + status);
-            if (status == BluetoothGatt.GATT_SUCCESS) {
+                case BluetoothGatt.GATT_REQUEST_NOT_SUPPORTED:
+                    Log.e(TAG, "onCharacteristicWrite: GATT_REQUEST_NOT_SUPPORTED");
+                    break;
 
-                byte[] read_data = characteristic.getValue();
-                String tempdata = new String(read_data);
-                mReadData = tempdata;
+                case BluetoothGatt.GATT_FAILURE:
+                    Log.e(TAG, "onCharacteristicWrite: GATT_FAILURE");
+                    break;
 
-                Log.d(TAG, "data = " + tempdata);
-                setStatus(BleStat.DATA_UPDATE);
-            } else {
+                case BluetoothGatt.GATT_INSUFFICIENT_AUTHENTICATION:
+                    break;
 
-                Log.e(TAG, "no_data!");
+                case BluetoothGatt.GATT_INSUFFICIENT_ENCRYPTION:
+                    break;
 
+                case BluetoothGatt.GATT_INVALID_OFFSET:
+                    break;
             }
 
+            setStatus(BleStat.READY);
         }
 
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, "onNotify");
+            if (DEVICE_NOTIFY_CHARACTERISTIC_UUID.equals(characteristic.getUuid().toString())) {
+                byte[] read_data = characteristic.getValue();
+                String notifyData = new String(read_data);
+                Log.e(TAG, notifyData);
+                mReadData = notifyData;
+                setStatus(BleStat.DATA_UPDATE);
+            }
+        }
     };
 
 }
